9e6468f8b04e197b4f3f4874bbaa46f4016a61b1
codingirl99 <105598595+codingirl99@users.noreply.github.com>
2025-11-11 22:46:00 -0600

    analysis file creation and data file

---
+++ notebooks/analysis.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#load the dataset
+df = pd.read_csv("notebooks/Tech_Use_Stress_Wellness.csv")
+
+#find the shape and data types
+print(df.shape())
+print(df.info())
+
+#viewing the first 5 rows
+print(df.head())
+
+# Summary statistics
+print(df.describe())
+
+# Check missing values
+print(df.isna().sum())
